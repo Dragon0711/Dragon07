ec014acb9c9f77095b0362ecf5753f59d7845dc7
Moustafa Moustafa <76392196+Moustafa-desha@users.noreply.github.com>
2022-01-21 18:02:16 +0300

    Update 2022_01_16_221351_create_admins_table.php

--- database/migrations/2022_01_16_221351_create_admins_table.php
+++ database/migrations/2022_01_16_221351_create_admins_table.php
@@ -20,7 +20,7 @@ class CreateadminsTable extends Migration
             $table->string('phone')->nullable();
             $table->foreignId('role_id')->default(0)->constrained();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('image')->nullable();
